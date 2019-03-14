8f13b29e38910a8a2371f585b7cb85998d44d495
aektos <ktosae@gmail.com>
2019-03-14 10:09:52 +0100

    [TASK] Add Authentication

--- site/src/Controller/FrontController.php
+++ site/src/Controller/FrontController.php
@@ -12,8 +12,6 @@ class FrontController extends AbstractController
      */
     public function index()
     {
-        return $this->render('front/index.html.twig', [
-            'controller_name' => 'FrontController',
-        ]);
+        return $this->render('front/index.html.twig', []);
     }
 }
